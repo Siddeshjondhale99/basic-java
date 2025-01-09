970cc16061168cac142ac416ff4045572604ccbf
Siddeshjondhale99 <160498310+Siddeshjondhale99@users.noreply.github.com>
2025-01-09 16:07:47 +0530

    Update JavaBasic.java
    i change this for basic

--- JavaBasic.java
+++ JavaBasic.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class JavaBasic {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in); // Correct input stream
+        Scanner sc = new Scanner(System.in); // Correct input s
         System.out.print("Enter the radius of the circle: ");
         float r = sc.nextFloat(); // Correct method
         float area = 3.14f * r * r; // Corrected area calculation
